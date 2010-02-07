5fb360ba2301a736e97b79b97fdb9e6a1dc268e0
lennart <l.melzer@tu-bs.de>
2010-02-07 15:38:30 +0100

    Prepare for switch to ecouch

--- src/feeds.erl
+++ src/feeds.erl
@@ -5,32 +5,35 @@
 
 read_all(_S) -> 
 	Feeds = blipdb:all(feeds),
-	lists:map(fun(F) -> {struct, [{<<"id">>, F#feed.id}, {<<"last_update">>, F#feed.last_update}]} end, Feeds).
+  Feeds.
+	%lists:map(fun(F) -> {struct, [{<<"id">>, F#feed.id}, {<<"last_update">>, F#feed.last_update}]} end, Feeds).
 
 create(S) ->
   {struct, Feed} = S,
-  Id = proplists:get_value(<<"id">>, Feed),
+  Id = lists:keyfind(<<"id">>,1, Feed),
 %  LastUpdate = proplists:get_value(<<"last_update">>, Feed),
-{atomic, ok} = blipdb:write(#feed{id= Id,last_update= calendar:now_to_universal_time(erlang:now())}),
+{atomic, ok} = blipdb:create(#feed{id= Id,last_update= calendar:now_to_universal_time(erlang:now())}),
 	Feed.
 
 read(S) ->
   {feed, Feed} = S,
-	Id = proplists:get_value(<<"id">>, Feed),
-
-	case blipdb:read({feed, Id}) of
-		[{feed, Id, LastUpdate}] ->
-      {struct, [{<<"id">>,Id},{<<"last_update">>, LastUpdate}]};
-		[] ->
+	case blipdb:read({feed, Feed#feed.id}) of
+		{feed, Feed} ->
+      {feed, Feed};
+		_ ->
 			{error, [{<<"message">>, <<"Feed not found">>}]}
 	end.
 
 update(S) ->
   {feed, Feed} = S,
-  {atomic, ok} = blipdb:write({feed, Feed#feed.id, calendar:now_to_universal_time(erlang:now())}),
-	{struct, [{<<"message">>, ok}]}.
+  case blipdb:update(Feed#feed.id, {feed, Feed#feed.id, calendar:now_to_universal_time(erlang:now())}) of
+  {feed, Feed} ->
+    {feed, Feed};
+  _ ->
+    {error, [{<<"message">>, <<"Feed update Failed">>}]}
+  end.
 
 delete(S) ->
   {feed, Feed} = S,
-	{atomic, ok} = blipdb:delete({feed, Feed#feed.id}),
+	{atomic, ok} = blipdb:delete(Feed#feed.id, {feed, Feed#feed.id}),
 	{struct, [{<<"message">>, ok}]}.
