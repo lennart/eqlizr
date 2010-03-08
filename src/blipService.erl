2420c6b0425bef9dd4705621b0bad02af29eb251
lennart <l.melzer@tu-bs.de>
2010-03-08 09:59:17 +0100

    Fixes for crashing feed synchronizer

--- src/blipService.erl
+++ src/blipService.erl
@@ -11,9 +11,23 @@ full_update() ->
   feed_synchronizer ! update.
 
 update_all_feeds(Interval) when is_integer(Interval) ->
+  process_flag(trap_exit, true),
+  spawn_link(?MODULE,update_all_feeds,[self(),Interval]),
+  receive
+    {'EXIT',_From,Reason} ->
+      io:format("Feed Synchronizer Crashed, restarting it"),
+      update_all_feeds(Interval);
+    {error, Reason} ->
+      io:format("Error: ~p~nRestarting Feed Synchronizer~n", [Reason]);
+      update_all_feeds(Interval);
+    done ->
+      io:format("Ending Work of the Feed Synchronizer")
+  end;
+
+update_all_feeds(Pid, Interval) ->
   case feeds:read_all() of
     {error, Reason} ->
-      io:format("Error: ~p~n", [Reason]);
+      Pid ! {error, Reason};
     Feeds ->
       io:format("Updating all feeds now, next time in ~p Minutes~n",[Interval/60]),
       lists:foreach(fun({feed,F}) -> feeds:fetch_blips({feed,F},true) end, Feeds)
@@ -22,7 +36,7 @@ update_all_feeds(Interval) when is_integer(Interval) ->
     update ->
       update_all_feeds(Interval);
     finished ->
-      io:format("Ending Work")
+      Pid ! done
   after Interval*1000 ->
       update_all_feeds(Interval)
   end.
